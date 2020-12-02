697ad9d40dd3146d366649f5425a3258c2b9b4f4
veselin9931 <ves.georgiev99@gmail.com>
2020-12-02 21:12:16 +0200

    I created new input models.

---
+++ MyBoutique.Infrastructure/InputModels/CreateCartInputModel.cs
@@ -0,0 +1,18 @@
+﻿using MyBoutique.Infrastructures.InputModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBoutique.Infrastructure.InputModels
+{
+    public class CreateCartInputModel
+    {
+        public virtual ICollection<CreateOrderInputModel> Orders { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal TotalPrice { get; set; }
+
+        public OrderDataInputModel OrderData { get; set; }
+    }
+}
---
+++ MyBoutique.Infrastructure/InputModels/OrderDataInputModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyBoutique.Infrastructure.InputModels
+{
+    public class OrderDataInputModel
+    {
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string Adress { get; set; }
+
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+
+        [Required]
+        [MaxLength(8)]
+        public string PromoCode { get; set; }
+    }
+}
