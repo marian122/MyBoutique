2de9377710f048034162bd4fcc04c6006fe29630
veselin9931 <ves.georgiev99@gmail.com>
2020-12-02 12:08:02 +0200

    Added Image and Model Table

---
+++ MyBoutique.Models/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyBoutique.Models
+{
+    public class Image
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string  Path { get; set; }
+
+        [Required]
+        public string Format { get; set; }
+
+        [Required]
+        public byte[] Content { get; set; }
+    }
+}
---
+++ MyBoutique.Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using MyBoutique.Common.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBoutique.Models
+{
+    public class Model : BaseDeletableModel<int>
+    {
+        public string  Size { get; set; }
+
+        public string Color { get; set; }
+    }
+}
