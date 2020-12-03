20c9f0b99266625e888010eea48e1086963d36a0
veselin9931 <ves.georgiev99@gmail.com>
2020-12-03 20:01:01 +0200

    ImageViewModel

---
+++ MyBoutique.Infrastructure/ViewModels/ImageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBoutique.Infrastructure.ViewModels
+{
+    public class ImageViewModel
+    {
+        public string Title { get; set; }
+
+        public string Path { get; set; }
+
+        public string Format { get; set; }
+    }
+}
