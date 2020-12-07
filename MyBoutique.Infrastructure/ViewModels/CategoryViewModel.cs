161fb18e5838a6fa4a324c8f4c64060636cdb98f
marian122 <maleksiev12@gmail.com>
2020-12-07 19:04:02 +0200

    dsad

---
+++ MyBoutique.Infrastructure/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MyBoutique.Mappings;
+using MyBoutique.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBoutique.Infrastructure.ViewModels
+{
+    public class CategoryViewModel : IMapFrom<CategoryType>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MyBoutique.Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBoutique.Services
+{
+    public interface ICategoryService
+    {
+        public Task<IEnumerable<TViewModel>> GetAllCategories<TViewModel>();
+        
+
+
+    }
+}
