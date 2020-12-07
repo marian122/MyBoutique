c0c5112ad52feeeb94639cb3ee4c00d67e700140
marian122 <maleksiev12@gmail.com>
2020-12-07 17:28:10 +0200

    removed categories bug occured

--- MyBoutique/ClientApp/src/app/app.module.ts
+++ MyBoutique/ClientApp/src/app/app.module.ts
@@ -23,17 +23,6 @@ import { KidProductListComponent } from './kid-product-list/kid-product-list.com
 import { DiscountProductListComponent } from './discount-product-list/discount-product-list.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { UnderwearComponent } from './male-product-list/underwear/underwear.component';
-import { FemaleBlouseComponent } from './female-product-list/female-blouse/female-blouse.component';
-import { FemaleShirtComponent } from './female-product-list/female-shirt/female-shirt.component';
-import { FemaleUnionsuitComponent } from './female-product-list/female-unionsuit/female-unionsuit.component';
-import { FemaleJacketsComponent } from './female-product-list/female-jackets/female-jackets.component';
-import { FemaleCoatsComponent } from './female-product-list/female-coats/female-coats.component';
-import { FemaleSkirtsComponent } from './female-product-list/female-skirts/female-skirts.component';
-import { FemalePantsComponent } from './female-product-list/female-pants/female-pants.component';
-import { FemaleSingletsComponent } from './female-product-list/female-singlets/female-singlets.component';
-import { FemaleTunicsComponent } from './female-product-list/female-tunics/female-tunics.component';
-import { FemaleScarvesComponent } from './female-product-list/female-scarves/female-scarves.component';
-import { FemaleUnderwearComponent } from './female-product-list/female-underwear/female-underwear.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,17 +38,6 @@ import { FemaleUnderwearComponent } from './female-product-list/female-underwear
     DiscountProductListComponent,
     AddProductComponent,
     UnderwearComponent,
-    FemaleBlouseComponent,
-    FemaleShirtComponent,
-    FemaleUnionsuitComponent,
-    FemaleJacketsComponent,
-    FemaleCoatsComponent,
-    FemaleSkirtsComponent,
-    FemalePantsComponent,
-    FemaleSingletsComponent,
-    FemaleTunicsComponent,
-    FemaleScarvesComponent,
-    FemaleUnderwearComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -77,9 +55,7 @@ import { FemaleUnderwearComponent } from './female-product-list/female-underwear
       { path: 'kid-products', component: KidProductListComponent },
       { path: 'discount-products', component: DiscountProductListComponent },
       { path: 'add-product', component: AddProductComponent },
-      { path: 'male-products/underwear', component: UnderwearComponent },
-      { path: 'female-products/blouses', component: FemaleBlouseComponent },
-      { path: 'female-products/underwear', component: FemaleUnderwearComponent }
+      { path: 'male-products/underwear', component: UnderwearComponent }
 
 
     ]),
