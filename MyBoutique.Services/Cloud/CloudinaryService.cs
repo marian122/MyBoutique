b0c3858906bc45bb1a25ab6294ba71b32f7cb5b6
veselin9931 <ves.georgiev99@gmail.com>
2020-12-16 14:31:41 +0200

    Added CloudinaryService for UploadImage

---
+++ MyBoutique.Services/Cloud/CloudinaryService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyBoutique.Services.Cloud
+{
+    public class CloudinaryService
+    {
+        private readonly CloudinaryDotNet.Cloudinary cloudinaryUtility;
+
+        public CloudinaryService(CloudinaryDotNet.Cloudinary cloudinaryUtility)
+        {
+            this.cloudinaryUtility = cloudinaryUtility;
+        }
+
+        public async Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName)
+        {
+            byte[] destinationData;
+
+            using (var ms = new MemoryStream())
+            {
+                await pictureFile.CopyToAsync(ms);
+                destinationData = ms.ToArray();
+            }
+
+            UploadResult uploadResult = null;
+
+            using (var ms = new MemoryStream(destinationData))
+            {
+                ImageUploadParams uploadParams = new ImageUploadParams
+                {
+                    Folder = "profile_images",
+                    File = new FileDescription(fileName, ms),
+                };
+
+                uploadResult = this.cloudinaryUtility.Upload(uploadParams);
+            }
+
+            return uploadResult?.SecureUri.AbsoluteUri;
+        }
+    }
+}
---
+++ MyBoutique.Services/Cloud/ICloudinaryService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MyBoutique.Services.Cloudinary
+{
+    public interface ICloudinaryService
+    {
+        public interface ICloudinaryService
+        {
+            Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName);
+        }
+    }
+}
